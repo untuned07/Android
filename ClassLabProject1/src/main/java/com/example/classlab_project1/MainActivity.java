e5e7610365e6b081a38e399e2b33ecadcf179733
Kartik Ginwal <114296676+untuned07@users.noreply.github.com>
2023-02-17 16:57:22 +0530

    Add files via upload

---
+++ ClassLabProject1/src/main/java/com/example/classlab_project1/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.classlab_project1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv=findViewById(R.id.textView);
+    }
+
+    public  void MyFunction(View view){
+        tv.setText("Final Text");
+    }
+
+
+}
