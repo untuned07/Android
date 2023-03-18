ceb24ebabe3076f1557cddf0dfb01d192fd1e6b6
Kartik Ginwal <114296676+untuned07@users.noreply.github.com>
2023-03-18 07:02:16 +0530

    Add files via upload

---
+++ Practical07/src/main/java/com/example/practical07/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.practical07;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    public int[] images = {
+            R.drawable.img1,
+            R.drawable.img2,
+            R.drawable.img3,
+            R.drawable.img4,
+            R.drawable.img5,
+            R.drawable.img6,
+            R.drawable.img7,
+            R.drawable.img8
+    };
+    ImageView imageView;
+    Button btn;
+    int i=1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.imageView);
+        btn = findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageView.setImageResource(images[i]);
+                ++i;
+                if(i==8){
+                    i=0;
+                }
+            }
+        });
+    }
+}
