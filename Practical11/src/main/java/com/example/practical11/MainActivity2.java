bf087de356d7622ec714bfa552f5da28b4287a09
Kartik Ginwal <114296676+untuned07@users.noreply.github.com>
2023-03-28 04:26:07 +0530

    Add files via upload

---
+++ Practical11/src/main/java/com/example/practical11/MainActivity2.java
@@ -0,0 +1,21 @@
+package com.example.practical11;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+    public void callFirstActivity(View view){
+        Intent i = new Intent(getApplicationContext(), MainActivity2.class);
+        startActivity(i);
+    }
+}
